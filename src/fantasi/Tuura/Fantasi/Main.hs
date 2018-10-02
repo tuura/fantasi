516ace5bed2cff066c8f29ba423d7b742b0beff4
jscott <baaquackmoo@gmail.com>
2018-10-02 17:00:50 +0100

    Addition of a -a flag which will cause Tuura.Fantasi.HubRewrite.hublAlias to rewrite the pangraph's largest degree vertex into 4 equivalently  connected Vertices.

--- src/fantasi/Tuura/Fantasi/Main.hs
+++ src/fantasi/Tuura/Fantasi/Main.hs
@@ -1,11 +1,14 @@
 module Tuura.Fantasi.Main (main) where
 
+import Pangraph
 import Tuura.Fantasi.Options
+import Tuura.Fantasi.HubRewrite     (aliasHub)
 import qualified Pangraph.GraphML.Parser  as P
 import qualified Tuura.Fantasi.VHDL.Writer     as VHDL
-import Data.ByteString  (readFile, writeFile)
+import Data.ByteString  (readFile, writeFile, ByteString)
 import Prelude hiding   (readFile, writeFile)
-import Data.Maybe       (maybe)
+import Data.Maybe       (fromMaybe)
+import Control.Monad(when)
 
 main :: IO ()
 main = do
@@ -14,9 +17,15 @@ main = do
     let graphMLPath           = optGraphML options
         graphVHDLPath         = optGraphName options
         simulationEnvVhdlPath = optSimName options
+        runAlias              = optAliasHub options
+
+    when runAlias (print "Running Alias")
 
     -- parse graph
-    pangraph <- ((maybe (error "file or graph is malformed") id) . P.parse) <$> readFile graphMLPath
+    maybePangraph <- (`getPangraph` options) <$> readFile graphMLPath
+    let pangraph = fromMaybe (error "Pangraph is nothing!") maybePangraph 
+    -- let Just pangraph =  maybePangraph 
+    
     let graphVHDL   = VHDL.writeGraph pangraph
     let simEnvVHDL  = VHDL.writeEnvironment pangraph
 
@@ -24,3 +33,11 @@ main = do
     writeFile graphVHDLPath graphVHDL
     -- output vhdl simulation environment
     writeFile simulationEnvVhdlPath simEnvVHDL
+
+getPangraph :: ByteString -> Options -> Maybe Pangraph
+getPangraph bs options =
+    P.parse bs >>= condition
+    where
+        condition = if optAliasHub options
+            then aliasHub
+            else Just 
