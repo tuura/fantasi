ae642e6a86f60b87a1d80eee0e959a9ecf509ea8
jscott <baaquackmoo@gmail.com>
2018-10-07 12:56:10 +0100

    Git add newest aliasHub algorithm, change implementation from FGL to Alga, move all aliasing code to HubRewrite.hs

--- src/Tuura/Fantasi/HubRewrite.hs
+++ src/Tuura/Fantasi/HubRewrite.hs
@@ -3,60 +3,49 @@
 module Tuura.Fantasi.HubRewrite where
 
 import Pangraph
-import qualified Pangraph.FGL as FGL
--- import qualified Pangraph.GraphML.Parser as GraphML
-
-import qualified Data.Graph.Inductive.Graph as FGL
-import qualified Data.Graph.Inductive.PatriciaTree as FGLTree
 
 import Data.ByteString.Char8 (pack)
 import Data.ByteString(ByteString)
 import qualified Data.ByteString as BS
 
-import Data.List(maximumBy)
+import Data.List(sortBy)
 
--- import Data.Maybe(fromJust)
-
--- import Data.Set(Set)
--- import qualified Data.Set as Set
-
--- import Control.Applicative((<*>))
-
-import Tuura.Fantasi.Alga
-
+import Algebra.Graph 
+import Algebra.Graph.ToGraph hiding(vertexList, edgeList)
 import qualified Algebra.Graph.ToGraph as Alga
 
--- | Used for a 'Set' implementation for 'Eq' and 'Ord' instances on endpoints only.
-newtype OrderEdgeByEnds = OrderEdgeByEnds Edge deriving (Show)
+import qualified Data.Set as Set
 
--- Eq on endpoints.
-instance Eq OrderEdgeByEnds where
-    (==) (OrderEdgeByEnds a) (OrderEdgeByEnds b) = edgeEndpoints a == edgeEndpoints b 
+import Data.Set(toList)
 
--- Ord on endpoints.
-instance Ord OrderEdgeByEnds where
-    compare (OrderEdgeByEnds a) (OrderEdgeByEnds b) = compare (edgeEndpoints a) (edgeEndpoints b)
+-- Given a hub vertex, and four new vertices, the function replaces the hub with the vertices.
+splitHub :: Ord a => a -> (a, a, a, a) -> Graph a -> Graph a
+splitHub x (x00, x01, x10, x11) g = overlays
+    [ removeVertex x g 
+    , biclique ai [x00, x01]
+    , biclique bi [x10, x11]
+    , biclique [x00, x10] ao
+    , biclique [x01, x11] bo ]
+  where
+    breakInHalf as =    let i = length as `div` 2
+                        in (take i as, drop i as) 
+    (ai, bi) = breakInHalf . toList $ preSet x g
+    (ao, bo) = breakInHalf . toList $ postSet x g
 
 -- Useful alias.
 type Endpoints = (VertexID, VertexID)
 
 -- Returns the largest degree node from a Pangraph.
 -- Uses FGL patrica tree graph.
-largestDegreeNode :: Pangraph -> VertexID
-largestDegreeNode p = let
-    -- Build a patricia tree backed FGL Graph instance.
-    fglGraph :: Pangraph -> FGLTree.Gr VertexID Int
-    fglGraph = uncurry FGL.mkGraph . FGL.convert
-    -- Calculate the order of all Vertices.
-    -- Return the VertexID of the maximum.
-    maxVertexOrder ::  FGLTree.Gr VertexID Int -> (VertexID, Int)
-    maxVertexOrder = maximumBy compareBySnd 
-        . map (\(a,b) -> (b, FGL.deg (fglGraph p) a)) . FGL.labNodes
-        where
-            compareBySnd :: (ByteString, Int) -> (ByteString, Int) -> Ordering
-            compareBySnd a b = compare (snd a) (snd b)
-    -- Combine together returning the VertexID which came from Pangraph
-    in (fst . maxVertexOrder . fglGraph) p
+largestDegreeNodes :: Graph VertexID -> [VertexID]
+largestDegreeNodes g = let
+    -- Get incoming Vertices of a Vertex.
+    sizePreSet = Set.size . \v -> preSet v g
+    -- Compare using Int instance.
+    ordVerticesBy :: VertexID -> VertexID -> Ordering
+    ordVerticesBy a b = sizePreSet a `compare` sizePreSet b
+    -- Sort the list largest to smallest using Flip
+    in sortBy (flip ordVerticesBy) . Alga.vertexList $ g
 
 -- Takes the a VertexID and generates its aliases based on a NxN matrix.
 generateAliasVertexIDs :: Int -> VertexID -> [VertexID]
@@ -70,14 +59,14 @@ generateAliasVertexIDs n bs = map (\i -> bs `BS.append` ps i) (generateMatrixCor
         generateMatrixCords n' = concatMap (\l -> zip (repeat l) [1..n']) [1..n']
 
 aliasHub :: Pangraph -> Maybe Pangraph
-aliasHub p = let 
-    hubID = largestDegreeNode p
-    aliasIDs = generateAliasVertexIDs 2 hubID
+aliasHub p = let
+    gOriginal = Alga.toGraph p
+    vID:_ = largestDegreeNodes gOriginal
+    aliasIDs = generateAliasVertexIDs 2 vID
     -- Is there a better way?
     aliasesAsTuples = (aliasIDs !! 0, aliasIDs !! 1, aliasIDs !! 2, aliasIDs !! 3)
-    g = splitHub hubID aliasesAsTuples (Alga.toGraph p)
-
-    in makePangraph (map makeVertex' . Alga.vertexList $ g) (map makeEdge' . Alga.edgeList $ g)  
+    gAliased = splitHub vID aliasesAsTuples gOriginal
+    in makePangraph (map makeVertex' . Alga.vertexList $ gAliased) (map makeEdge' . Alga.edgeList $ gAliased)
 
 makeVertex' :: VertexID -> Vertex
 makeVertex' vID = makeVertex vID [("id", vID)]
